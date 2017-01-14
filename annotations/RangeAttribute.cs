d885874b7d12632ae1fcb8722f46628475a6121f
Kasper Tvede <kasper@webmasteren.eu>
2017-01-15 00:53:07 +0100

    Added Range annotation

---
+++ annotations/RangeAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter |
+                AttributeTargets.ReturnValue)]
+public class RangeAttribute : Attribute
+{
+    private readonly int _minValue;
+    private readonly int _maxValue;
+
+    public RangeAttribute(int minValue, int maxValue) // url is a positional parameter
+    {
+        _minValue = minValue;
+        _maxValue = maxValue;
+    }
+}
+
+[AttributeUsage(AttributeTargets.All)]
+public class PositiveIntAttribute : RangeAttribute
+{
+    public PositiveIntAttribute() : base(0, int.MaxValue)
+    {
+    }
+}
