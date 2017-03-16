75f69161892b9179a34138add5e86757c1ab6aef
Kasper Tvede <kasper@webmasteren.eu>
2017-03-17 00:22:21 +0100

    finished the number values from strings. and more goodies

--- extensions/DictonaryExtensions.cs
+++ extensions/DictonaryExtensions.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Xml;
 using CommonSenseCSharp.datastructures;
 using CommonSenseCSharp.extensions;
 using JetBrains.Annotations;
 
-public static class DictonaryUtil{
-
+public static class DictonaryUtil {
     public static T GetSafe<T, TK>([NotNull] this Dictionary<TK, T> dict, [NotNull] TK lookup) {
         return dict.GetSafe(lookup, default(T));
     }
 
     public static T GetSafe<T, TK>([NotNull] this Dictionary<TK, T> dict, [NotNull] TK lookup,
-        [CanBeNull] T fallback){
+        [CanBeNull] T fallback) {
         return dict.ContainsKey(lookup) ? dict[lookup] : fallback;
     }
 
@@ -21,79 +22,84 @@ public static class DictonaryUtil{
     }
 
     public static void UseValue<T, TK>([NotNull] this Dictionary<TK, T> dict, [NotNull] TK lookup,
-        [NotNull] Action<T> onAction){
+        [NotNull] Action<T> onAction) {
         dict.GetSafe(lookup)?.IfSafe(onAction);
     }
 
     public static void UseAndFlatTransform<T, TK, TU>([NotNull] this Dictionary<TK, T> dict, [NotNull] TK lookup,
-        [NotNull] Func<T, TU> transform, [NotNull] Action<TU> onSuccess){
+        [NotNull] Func<T, TU> transform, [NotNull] Action<TU> onSuccess) {
         dict.FlatPerformIfContainsKey(lookup, x => { transform(x)?.IfSafe(onSuccess); });
     }
 
     [NotNull]
     public static TU UseAndFlatTransform<T, TK, TU>([NotNull] this Dictionary<TK, T> dict, [NotNull] TK lookup,
-        [NotNull] Func<T, TU> transform) where TU : struct{
+        [NotNull] Func<T, TU> transform) where TU : struct {
         return dict.GetSafe(lookup).IfSafe(transform);
     }
 
     [NotNull]
     public static TU UseAndFlatTransform<T, TK, TU>([NotNull] this Dictionary<TK, T> dict, [NotNull] TK lookup,
-        [NotNull] Func<T, TU> transform, [NotNull] TU defaultValue) where TU : class{
+        [NotNull] Func<T, TU> transform, [NotNull] TU defaultValue) where TU : class {
         return dict.GetSafe(lookup).IfSafe(transform) ?? defaultValue;
     }
 
 
     public static void AddAll<TK, T>([NotNull] this Dictionary<TK, T> dict, [NotNull] Func<T, TK> transformer,
-        [CanBeNull] params T[] items){
+        [CanBeNull] params T[] items) {
         items?.FlatForeach(item => dict.Add(transformer(item), item)); //todo flatmap first ??? or too slow ?
     }
 
     public static void PerformIfContains<TK, T>([NotNull] this Dictionary<TK, T> dict, [NotNull] TK key,
-        [NotNull] Action<T> onContains){
-        if (dict.ContainsKey(key)){
+        [NotNull] Action<T> onContains) {
+        if (dict.ContainsKey(key)) {
             onContains(dict[key]);
         }
     }
 
     public static void FlatPerformIfContainsKey<TK, T>([NotNull] this Dictionary<TK, T> dict, [NotNull] TK key,
-        [NotNull] Action<T> onContains){
-        if (dict.ContainsKey((key))){
+        [NotNull] Action<T> onContains) {
+        if (dict.ContainsKey((key))) {
             dict[key]?.IfSafe(onContains);
         }
     }
 
     public static void FlatPerformIfContainsKey<TK, T>([NotNull] this Dictionary<TK, T> dict, [NotNull] TK key,
-        [NotNull] Action<T> onContains, [NotNull] Action ifNot){
-        if (dict.ContainsKey((key))){
+        [NotNull] Action<T> onContains, [NotNull] Action ifNot) {
+        if (dict.ContainsKey((key))) {
             dict[key]?.IfSafe(onContains);
-        }
-        else{
+        } else {
             ifNot();
         }
     }
 
-    public static void Add<TK, T>([NotNull] this Dictionary<TK, T> dict, KeyValuePair<TK, T> data){
+    public static void Add<TK, T>([NotNull] this Dictionary<TK, T> dict, KeyValuePair<TK, T> data) {
         dict.Add(data.Key, data.Value);
     }
 
     [NotNull]
     public static Dictionary<TK, T> AddF<TK, T>([NotNull] this Dictionary<TK, T> dict, [NotNull] TK key,
-        [NotNull] T value){
+        [NotNull] T value) {
         dict.Add(key, value);
         return dict;
     }
 
     [NotNull]
     public static Dictionary<string, T> RemoveAll<T>([NotNull] this Dictionary<string, T> dict,
-        [CanBeNull] params string[] toRemove){
+        [CanBeNull] params string[] toRemove) {
         toRemove?.FlatForeach(dict.Remove);
         return dict;
     }
 
+    [CanBeNull]
+    public static T GetAndRemove<TK, T>([NotNull] this Dictionary<TK, T> dict,[NotNull] TK key) {
+        var temp = dict.GetSafe(key);
+        dict.Remove(key);
+        return temp;
+    }
 
     public static void InsertInto<TKey, TValue, U>([NotNull] this Dictionary<TKey, U> dict,
-        [NotNull] TKey key, [NotNull] TValue value, Func<U> listCreator) where U : IList<TValue>{
-        if (!dict.ContainsKey(key)){
+        [NotNull] TKey key, [NotNull] TValue value, Func<U> listCreator) where U : IList<TValue> {
+        if (!dict.ContainsKey(key)) {
             dict.Add(key, listCreator());
         }
         dict[key].Add(value);
--- extensions/StringHelper.cs
+++ extensions/StringHelper.cs
@@ -1,55 +1,63 @@
 ﻿using System;
 using System.Text;
+using JetBrains.Annotations;
 
-public static class StringBuilderExtensions
-{
+public static class StringBuilderExtensions {
     /// <summary>
     /// chops off the request amount of chars, and if not possible (eg the stringbuilder does not have that many chars) it returns an empty string.
     /// </summary>
     /// <param name="builder"></param>
     /// <param name="chopCharsOffAtEnd"></param>
     /// <returns></returns>
-    public static string ToStringSafeEnd(this StringBuilder builder, int chopCharsOffAtEnd)
-    {
-        if (builder.Length > chopCharsOffAtEnd)
-        {
+    public static string ToStringSafeEnd(this StringBuilder builder, int chopCharsOffAtEnd) {
+        if (builder.Length > chopCharsOffAtEnd) {
             return builder.ToString(0, builder.Length - chopCharsOffAtEnd);
-        }
-        else
-        {
+        } else {
             return "";
         }
     }
+
     //// str - the source string
     //// index- the start location to replace at (0-based)
     //// length - the number of characters to be removed before inserting
     //// replace - the string that is replacing characters
-    public static string ReplaceAt(this string str, int index, int length, string replace)
-    {
+    public static string ReplaceAt(this string str, int index, int length, string replace) {
         return str.Remove(index, Math.Min(length, str.Length - index))
-                .Insert(index, replace ?? "");
+            .Insert(index, replace ?? "");
     }
-    
-    public static string AppendIf(this string str, string otherStr, bool shouldAppend)
-    {
-        if (shouldAppend)
-        {
+
+    public static string AppendIf(this string str, string otherStr, bool shouldAppend) {
+        if (shouldAppend) {
             return str + otherStr;
         }
         return str;
     }
 
-    public static int IntValue(this string str)
-    {
+    public static int IntValue([NotNull] this string str) {
         var temp = 0;
         int.TryParse(str, out temp);
         return temp;
     }
 
-    public static StringBuilder Append(this StringBuilder builder, params string[] items)
-    {
+    public static int IntValue([NotNull] this string str, int otherWise) {
+        return int.TryParse(str, out var temp) ? temp : otherWise;
+    }
+
+    public static float FloatValue([NotNull] this string str, float otherWise) {
+        return float.TryParse(str, out var temp) ? temp : otherWise;
+    }
+
+    public static long LongValue([NotNull] this string str, long otherWise) {
+        return long.TryParse(str, out var temp) ? temp : otherWise;
+    }
+
+    public static double IntValue([NotNull] this string str, double otherWise) {
+        return double.TryParse(str, out var temp) ? temp : otherWise;
+    }
+
+    [NotNull]
+    public static StringBuilder Append([NotNull] this StringBuilder builder, [NotNull] params string[] items) {
         items.FlatForeach(builder.Append);
         return builder;
     }
-
 }
